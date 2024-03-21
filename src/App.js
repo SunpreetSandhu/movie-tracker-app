8c67522cf53ae0589eacf58bb1e3266d16a467e9
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-03-21 14:58:41 -0400

    fixed prop drilling for movie list

--- src/App.js
+++ src/App.js
@@ -55,18 +55,29 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies}>
-        <Logo />
+      <NavBar>
         <Search />
         <NumResults movies={movies} />
       </NavBar>
-      <Main movies={movies} />
+      <Main>
+        <ListBox movies={movies}>
+          {" "}
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
 function NavBar({ children }) {
-  return <nav className="nav-bar">{children}</nav>;
+  return (
+    <nav className="nav-bar">
+      {" "}
+      <Logo />
+      {children}
+    </nav>
+  );
 }
 
 function NumResults({ movies }) {
@@ -99,16 +110,11 @@ function Search() {
   );
 }
 
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
   return (
     <div className="box">
@@ -118,7 +124,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
