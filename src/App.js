be918dc7adf4dbf8c6271629147376d1894738ff
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-03-30 16:02:21 -0400

    fixed bug when search box is highlighted the movie list does not clear when enter is presed only when its not the active element

--- src/App.js
+++ src/App.js
@@ -215,6 +215,9 @@ function Search({ query, setQuery }) {
   const inputEl = useRef(null);
   useEffect(function () {
     function callback(e) {
+      if (document.activeElement === inputEl.current) {
+        return;
+      }
       if (e.code === "Enter") {
         inputEl.current.focus();
         setQuery("");
